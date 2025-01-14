139661dfb945d2e06cad7f47fdeeeaac02313234
Louis Richard <louis.richard@edu.devinci.fr>
2025-01-14 14:36:35 +0100

    Add few letters

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 from distutils.core import setup
 setup(
- name='{name-of-your-project}',
+ name='{ASCII_Art_Generator_Louis_RICHARD_CDOF5}',
  version='1.0',
- author='{your-full-name-or-your-github-username}',
+ author='{Louis_RICHARD}',
  license='MIT',
  long_description=open('README.md').read(),
 )
